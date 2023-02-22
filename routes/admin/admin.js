fa1a3f0b7cc791035d0b722c462439c6ad75b4cb
Dawid Bryja <dawbryja@gmail.com>
2023-02-22 12:45:49 +0100

    Revert "update admin signin"
    This reverts commit 05c055c2f6562c7f1056952799b7cd41c3699edf.

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,41 +1,54 @@
 import express from "express";
-import { makeDb } from "../../repositories/db.js";
+import { db } from "../../repositories/db.js";
 
 const router = express.Router();
 
-const db = makeDb({
-  host: "localhost",
-  user: "root",
-  database: "buddyexplorer",
-});
-
 // login as an administrator
 router.get("/admin", (req, res) => {
-  res.render("admin/signInTemplate", { data: { error: false } });
+  res.render("admin/signInTemplate");
 });
 
-router.post("/admin", async (req, res) => {
+router.post("/admin", (req, res) => {
   const { login, password } = req.body;
 
   let sql = `SELECT 'adminID' FROM admins WHERE login='${login}' AND password=MD5('${password}') `;
-  try {
-    const result = await db.query(sql);
+  db.query(sql, (err, result) => {
+    if (err) {
+      res.redirect("/admin");
+      throw err;
+    }
+    console.log(req.body);
     const data = JSON.parse(JSON.stringify(result));
     req.session.adminID = data[0].adminID;
-    res.redirect("/admin/panel");
-  } catch (err) {
-    res.render("admin/signInTemplate", { data: { error: "Zły login bądź hasło" } });
-    return;
-  }
+    res.send(req.session.adminID);
+  });
 });
 
 // admin panel
 router.get("/admin/panel", (req, res) => {
-  if (!req.session.adminID) {
-    res.redirect("/admin");
-  } else {
-    res.render("admin/panelTemplate");
-  }
+  res.render("admin/panelTemplate");
 });
+
+router.post("/admin/panel", (req, res) => {
+  res.send(req.body);
+});
+
+// if (!req.session.adminID) {
+//    return res.redirect("/admin");
+//  } else {
+//    next();
+//  }
+
+// router.post(
+//    "/signup",
+//    [requireEmail, requirePassword, requireConfirm],
+//    handleErrors(signupTemplate),
+//    async (req, res) => {
+//      const { email, password } = req.body;
+//      const user = await usersRepo.create({ email, password });
+//      req.session.userId = user.id;
+//      res.redirect("/admin/products");
+//    }
+//  );
 
 export const adminRouter = router;
